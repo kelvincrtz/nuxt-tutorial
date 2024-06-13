8dad7cc4da54bd8dd23f699866cd4ed2109ddb8e
Kelvin Cortez <kelvcortez@gmail.com>
2024-06-13 21:58:23 +1200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <iframe src="https://www.elegantthemes.com" height="900" width="1300"></iframe>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>    
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, ratione animi? Explicabo ut provident fugiat dignissimos reprehenderit impedit incidunt distinctio!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, facere illum assumenda consequuntur sed fugit numquam harum nostrum quidem vitae.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
